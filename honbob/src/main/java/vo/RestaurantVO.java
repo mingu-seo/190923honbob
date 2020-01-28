43a94893ecdb288dadaf8715b4f8b1020f4b079f
qhdqhdgh <59349126+qhdqhdgh@users.noreply.github.com>
2020-01-28 18:15:44 +0900

    Add files via upload

--- honbob/src/main/java/vo/RestaurantVO.java
+++ honbob/src/main/java/vo/RestaurantVO.java
@@ -1,7 +1,7 @@
-package vo;
+package Honmuk;
 
 public class RestaurantVO {
-	//식당 번호
+
 	private int res_num;
 	//식당이름
 	private String res_name;
@@ -20,6 +20,134 @@ public class RestaurantVO {
 	//별점한 사람
 	private int gradecount;
 	
+	
+	//한식
+	private int koreafood;
+	//즁식
+	private int chinafood;
+	//일식
+	private int japanfood;
+	//양식
+	private int westernfood;
+	//야식
+	private int etcfood;
+	
+	
+	//혼술가능
+	private int drink;
+	//칸막이
+	private int partition2;
+	//무인계산기
+	private int calculator;
+	//주차
+	private int park;
+	//2인테이블
+	private int table2;	
+	
+	
+	private int page;
+	private int limit;
+	private int startIndex;	
+	
+	private String keyword;
+		
+	
+	public String getKeyword() {
+		return keyword;
+	}
+
+	public void setKeyword(String keyword) {
+		this.keyword = keyword;
+	}
+
+	public RestaurantVO() {
+		this.page=1; 
+		this.limit=10;
+	}
+	
+	public int getStartIndex() {
+		int i = (this.page-1)*this.limit;
+		return i;
+	}
+
+	public void setStartIndex(int startIndex) {
+		this.startIndex = startIndex;
+	}
+
+	public int getPage() {
+		return page;
+	}
+	public void setPage(int page) {
+		this.page = page;
+	}
+	public int getLimit() {
+		return limit;
+	}
+	public void setLimit(int limit) {
+		this.limit = limit;
+	}	
+	
+	
+	public int getKoreafood() {
+		return koreafood;
+	}
+	public void setKoreafood(int koreafood) {
+		this.koreafood = koreafood;
+	}
+	public int getChinafood() {
+		return chinafood;
+	}
+	public void setChinafood(int chinafood) {
+		this.chinafood = chinafood;
+	}
+	public int getJapanfood() {
+		return japanfood;
+	}
+	public void setJapanfood(int japanfood) {
+		this.japanfood = japanfood;
+	}
+	public int getWesternfood() {
+		return westernfood;
+	}
+	public void setWesternfood(int westernfood) {
+		this.westernfood = westernfood;
+	}
+	public int getEtcfood() {
+		return etcfood;
+	}
+	public void setEtcfood(int etcfood) {
+		this.etcfood = etcfood;
+	}
+	public int getDrink() {
+		return drink;
+	}
+	public void setDrink(int drink) {
+		this.drink = drink;
+	}
+	public int getPartition2() {
+		return partition2;
+	}
+	public void setPartition2(int partition) {
+		this.partition2 = partition;
+	}
+	public int getCalculator() {
+		return calculator;
+	}
+	public void setCalculator(int calculator) {
+		this.calculator = calculator;
+	}
+	public int getPark() {
+		return park;
+	}
+	public void setPark(int park) {
+		this.park = park;
+	}
+	public int getTable2() {
+		return table2;
+	}
+	public void setTable2(int table) {
+		this.table2 = table;
+	}
 	public int getRes_num() {
 		return res_num;
 	}
@@ -74,6 +202,4 @@ public class RestaurantVO {
 	public void setGradecount(int gradecount) {
 		this.gradecount = gradecount;
 	}
-	
-	
 }
