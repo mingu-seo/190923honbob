40611c62cb9395f66ce08578b1502cb9ce99df41
qhdqhdgh <59349126+qhdqhdgh@users.noreply.github.com>
2020-01-29 11:46:30 +0900

    Add files via upload

---
+++ honbob/src/main/java/util/Page.java
@@ -0,0 +1,35 @@
+package util;
+
+import Honmuk.RestaurantVO;
+import Honmuk.PageInfo;
+
+public class Page {
+	
+	public PageInfo page(String page, int listCount) {
+		
+		RestaurantVO vo = new RestaurantVO();
+		
+		if(page != null) {
+			vo.setPage(Integer.parseInt(page));
+		}
+		int p = vo.getPage();
+		int limit = vo.getLimit();
+
+		int maxPage = listCount/limit;
+		int startPage = (p-1)/limit*limit+1;		
+		int endPage = startPage+10-1;
+		
+		if(endPage>maxPage) endPage=maxPage;
+		
+		PageInfo pageInfo = new PageInfo();
+		pageInfo.setEndPage(endPage);
+		pageInfo.setListCount(listCount);
+		pageInfo.setMaxPage(maxPage);
+		pageInfo.setPage(p);
+		pageInfo.setStartPage(startPage);		
+		
+		return pageInfo;
+	}
+
+	
+}
