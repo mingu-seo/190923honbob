690320f8fa15aaceb8269e9e46703e372e521661
qhdqhdgh <59349126+qhdqhdgh@users.noreply.github.com>
2020-01-29 11:39:19 +0900

    Update HonbobDetailService.java

--- honbob/src/main/java/service/HonbobDetailService.java
+++ honbob/src/main/java/service/HonbobDetailService.java
@@ -1,8 +1,20 @@
 package service;
 
 import org.springframework.stereotype.Service;
+import java.util.List;
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
-public class HonbobDetailService {
+public class HonmukDetailService {
 	
+	@Autowired
+	HonmukDAO HonmukDao;
+	
+	public List<RestaurantVO> searchList(RestaurantVO resVO){		
+		return HonmukDao.searchList(resVO);
+	}
+	
+	public int count() {
+		return HonmukDao.count();
+	}
 }
