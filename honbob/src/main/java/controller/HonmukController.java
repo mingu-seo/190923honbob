38b83c1d961bfab8a2dea6226b9587b9df2d2b1e
gd5 <gd5@DESKTOP-LVUTDF3>
2020-02-13 16:21:48 +0900

    context-servlet 수정

--- honbob/src/main/java/controller/HonmukController.java
+++ honbob/src/main/java/controller/HonmukController.java
@@ -1,6 +1,5 @@
 package controller;
 
-<<<<<<< HEAD
 import java.io.Console;
 import java.io.IOException;
 import java.io.PrintWriter;
@@ -11,9 +10,7 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 
 
-=======
 import mail.SendMail;
->>>>>>> branch 'master' of https://github.com/mingu-seo/190923honbob.git
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
 import org.springframework.ui.Model;
